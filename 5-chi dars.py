fc49ef54f34d56fa903cbeb12a27f82cd4104ced
jahongir <jahongirrajabov0515@gmail.com>
2026-03-18 00:03:14 +0500

    dars

---
+++ 5-chi dars.py
@@ -0,0 +1,12 @@
+mevalar = ["olma", "anjir", "anor", "nok"]
+#mevalar.insert(2,"tarvuz")  #insertni bilan so'zni xoxlagan joyga qoysa bo'ladi.
+#mevalar.append("gul")  #append bilan so'zni qo'shsak bo'ladi.
+#del mevalar[2]   #del bilan so'zlarni o'chirsak bo'ladi.
+#mevalar.remove("anjir")   #remove bilan xoxlagan so'zni nomini yozib chiqarib yuborsak bo'ladi.
+#print(mevalar)
+
+
+bozorlik = ["go'sht", "banan", "yog'", "piyoz"]
+mahsulotlar = bozorlik.pop(1)
+print("men " + mahsulotlar + " sotib oldim")
+print("olinmagan mahsulotlar", bozorlik)
