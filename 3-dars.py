38307a6beb7705120aac0ea8f5d4c2bea491cd8d
jahongir <jahongirrajabov0515@gmail.com>
2026-03-17 07:56:28 +0500

    dars

---
+++ 3-dars.py
@@ -0,0 +1,27 @@
+#ism = 'Jahongir'
+#print("mening ismim " + ism)
+
+#ism = 'Jahongir'
+#familiya = 'Rajabov'
+#print(ism + ' ' + familiya)
+
+
+#ism = 'Jahongir'
+#familiya = 'Rajabov'
+#ism_familiya = f"{ism} {familiya}"
+#print(ism_familiya)
+
+#ism = 'Jahongir'
+#familiya = 'Rajabov'
+#print(f"mening ismim {ism}. {familiya} {ism}")
+
+#ism = 'Jahongir'
+#familiya = 'Rajabov'
+#ism_sharif = f"{ism} {familiya}"
+#print(ism_sharif.title())
+#print(ism_sharif.lower())
+#print(ism_sharif.capitalize())
+
+
+ism = input("ismingizni kiriting\n===")
+print("assalomu aleykum," + ism.title())
