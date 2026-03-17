f4a5c1a2f4115961fd6c6d6a46109bf9d5822f10
jahongir <jahongirrajabov0515@gmail.com>
2026-03-17 23:10:51 +0500

    dars

---
+++ 4-dars.py
@@ -0,0 +1,9 @@
+#ism = 'Jahongir'
+#yosh = 18
+#xabar = ism + ' ' + str(yosh) + ' ' + 'yoshda'
+#print(xabar)
+
+
+yil = int(input("tug'ulgan yilingizni kiriting:"))
+yosh = 2026 - yil
+print("siz", yosh, "yoshda ekansiz")
