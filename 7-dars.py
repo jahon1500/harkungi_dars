66775ac4e975c92887667202cdd39b26322b6222
jahongir <jahongirrajabov0515@gmail.com>
2026-03-18 07:59:17 +0500

    dars

---
+++ 7-dars.py
@@ -0,0 +1,16 @@
+#mehmonlar = ['ali', 'vali', 'hasan', 'husan']
+#for mehon in mehmonlar:
+#    print("salom", mehon)
+
+
+#ehmonlar = ['ali', 'vali', 'hasan', 'husan']
+#for mehmon in mehmonlar:
+#    print(f"hurmatli {mehmon}, sizni taklif qiliib qolaman")
+#  print("hurmat bilan ushalar")
+
+
+dostlar = []
+print("dostingizni ismi nima")
+for n in range(3):
+    dostlar.append(input(f"{n+1}-dostingizni ismini kiriting"))
+    print(dostlar)
