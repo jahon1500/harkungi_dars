9230e8e04ecac5ad5d381dcb227a5cbad56a7287
jahongir <jahongirrajabov0515@gmail.com>
2026-03-25 23:44:19 +0500

    dars

---
+++ 9-dars.py
@@ -0,0 +1,56 @@
+# son = -45
+# if son<0:
+#     print("manfiy son")
+# else:
+#     print("musbat")
+
+
+# yosh = int(input("yoshingizni kiriting"))
+# if yosh <=5:
+#     print("sizga kirish bepul")
+# elif yosh<=10:
+#     print("sizga kirish 10000 so'm")
+# else:
+#     print("sizga kirish mumkin emas")
+
+
+
+# yosh = int(input("yoshingiz nechida"))
+# if yosh <=5:
+#     price = 0
+# elif yosh <=10:
+#     price = 10
+# elif yosh <=15:
+#     price = 10000
+# else:
+#     price = 20000
+#     print(f"sizga kirish {price} so'm")
+
+
+# price = 20000
+# choy = True
+# non = True
+# salat = False
+# if choy and non and salat:
+#     price = price +15000
+# elif choy or non or salat:
+#
+#     print(f"jami {price} so'm")    price = price + 10000
+
+
+menu = ['osh', 'kabob', 'gosht', 'manti', 'qazi']
+ovqat = input("nima buyurma qilasiz")
+if ovqat.lower () in menu:
+    print("buyurtma qabul qilindi")
+else:
+    print("buyutma bekor qilindi")
+
+
+
+
+
+
+
+
+
+
