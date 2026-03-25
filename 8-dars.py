023bd649f44a265c4d4c12315272855eaa548c27
jahongir <jahongirrajabov0515@gmail.com>
2026-03-25 07:38:21 +0500

    dars

---
+++ 8-dars.py
@@ -0,0 +1,26 @@
+#cars = ['bmw', 'mersedez', 'kia']
+#for car in cars:
+#    if car == 'bmw' :
+#        print(car.upper())
+#    else:
+ #       print(car.title())
+
+
+#ism = input("ismingizni kiriting")
+#if ism.lower() != "ali":
+#    print(f"uzur, {ism.title()} biz alini qidiryapmiz")
+#else:
+#    print("salom ali")
+
+
+#javob = float(input("12*6 nechiga teng"))
+#if javob !=72:
+#    print("javob xato")
+
+
+yil = int(input("yoshingizni kiriting"))
+if 2026-yil<18:
+    print(f"yoshingiz {2026-yil}da ekan")
+    print("kirish mumkin emas")
+else:
+    print("xush kelibsiz")
