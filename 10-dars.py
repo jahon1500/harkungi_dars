8669b9fa159aec4ed93de5c1d1e52ff090b2fb7c
jahongir <jahongirrajabov0515@gmail.com>
2026-03-26 22:37:17 +0500

    dars

---
+++ 10-dars.py
@@ -0,0 +1,6 @@
+# cars = {'lacetti':10000, 'captiva':20000, 'malibu':30000, 'tico':1000, 'cobalt':15000}
+# print(f"moshina narhi {cars['tico']}")
+
+
+shaxs = {'ism':'rajabov jahongir',"tugulgan yil":2007, 'yosh':18}
+print(f"{shaxs['ism'].title()},{shaxs['tugulgan yil']}-yilda tugulgan, {shaxs['yosh']}yoshda")
